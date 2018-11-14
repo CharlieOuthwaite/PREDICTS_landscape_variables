92ce51e2a590c6065c5e7d872e7f63de6b2eab06
CharlieOuthwaite <charlotteouthwaite@gmail.com>
2018-11-14 11:36:32 +0000

    Added first attempt at extracting forest biome information.  Currently using sf package but might change this later.

--- Analysis_workflow/Yield_Forest_Workflow.r
+++ Analysis_workflow/Yield_Forest_Workflow.r
@@ -81,8 +81,87 @@ length(unique(pred.sites.crop$Best_guess_binomial)) # 8816 species
 #                                                          #
 ############################################################
 
+# load in the biome dataset
+
+# read in the biomes shapefile (this has 4 biomes in, just for forest types?)
+# humid tropics, dry tropics, temperate, boreal
+biomes <- st_read(paste0(datadir, "/biomes/biomes.shp")) %>%
+  st_set_crs(4326) # setting the CRS to world lat/longs
+
+# removing the extra grid code column
+biomes <- biomes[2:3]
+
+# basic plot of the biomes
+plot(biomes)
+
+# first get the biomes for the site level data
+pred.sites.metrics.crop$biome <- apply(pred.sites.metrics.crop[ , c("Longitude", "Latitude")], 1, function(x) {  
+  # transformation to palnar is required, since sf library assumes planar projection 
+  
+  pred.sf <- st_transform(st_sfc(st_point(x),crs = 4326), 4326)
+  # st_intersects with sparse = FALSE returns a logical matrix
+  # with rows corresponds to argument 1 (points) and 
+  # columns to argument 2 (polygons)
+  
+  biomes[which(st_intersects(pred.sf, biomes, sparse = FALSE)), ]$BIOME 
+})
+
+# rename biome values from numbers
+levels(biomes$BIOME)
+pred.sites.metrics.crop$biome <- sub(1, "Boreal", pred.sites.metrics.crop$biome)
+pred.sites.metrics.crop$biome <- sub(2, "Dry Tropics", pred.sites.metrics.crop$biome)
+pred.sites.metrics.crop$biome <- sub(3, "Humid Tropics", pred.sites.metrics.crop$biome)
+pred.sites.metrics.crop$biome <- sub(4, "Temperate", pred.sites.metrics.crop$biome)
 
 
+ggplot()+
+  borders("world", colour = "black", size = 0.3) +
+  theme_map() +
+  geom_sf(data = biomes, aes(fill = BIOME), alpha = 0.5) +
+  geom_point(data = pred.sites.metrics.crop, aes(x = Longitude, y = Latitude, col = biome)) +
+  theme(panel.grid.major = element_line(colour = "transparent"))
+
+
+# remove sites that are not within a forest biome
+nrow(pred.sites.metrics.crop[pred.sites.metrics.crop$biome == "integer(0)",])
+
+pred.sites.metrics.crop <- pred.sites.metrics.crop[!pred.sites.metrics.crop$biome == "integer(0)",]
+
+
+
+
+# then get the biomes for the species level data
+pred.sites.crop$biome <- apply(pred.sites.crop[ , c("Longitude", "Latitude")], 1, function(x) {  
+  # transformation to palnar is required, since sf library assumes planar projection 
+  
+  pred.sf <- st_transform(st_sfc(st_point(x),crs = 4326), 4326)
+  # st_intersects with sparse = FALSE returns a logical matrix
+  # with rows corresponds to argument 1 (points) and 
+  # columns to argument 2 (polygons)
+  
+  biomes[which(st_intersects(pred.sf, biomes, sparse = FALSE)), ]$BIOME 
+})
+
+# rename biome values from numbers
+levels(biomes$BIOME)
+pred.sites.crop$biome <- sub(1, "Boreal", pred.sites.crop$biome)
+pred.sites.crop$biome <- sub(2, "Dry Tropics", pred.sites.crop$biome)
+pred.sites.crop$biome <- sub(3, "Humid Tropics", pred.sites.crop$biome)
+pred.sites.crop$biome <- sub(4, "Temperate", pred.sites.crop$biome)
+
+# remove sites that are not within a forest biome
+nrow(pred.sites.crop[pred.sites.crop$biome == "integer(0)",])
+
+pred.sites.crop <- pred.sites.crop[!pred.sites.crop$biome == "integer(0)",]
+
+# dataset summaries
+
+# nstudies
+length(unique(pred.sites.crop$SS)) # 141 studies
+# nsites
+length(unique(pred.sites.crop$SSS)) # 2676 sites
+# nspecies
+length(unique(pred.sites.crop$Best_guess_binomial)) # 8585 species
 
 
 
