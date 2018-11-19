a72652ab3c5d444a64b6ea30351733d505a180e5
CharlieOuthwaite <charlotteouthwaite@gmail.com>
2018-11-19 11:28:40 +0000

    Continuing to get to grips with the crop and biomes data.

--- General_code/Biomes_forest_cover,r.R
+++ General_code/Biomes_forest_cover,r.R
@@ -11,22 +11,31 @@
 # biome dataset and forest cover dataset from the above website, figuring out how to read
 # them in and use with the PREDICTS data, once done, add into a main script/workflow
 
-
 rm(list = ls())
+
+# load libraries
+library(sf)
+library(ggplot2)
+library(ggthemes)
 
 # where are the datasets?
 datadir <- "D:/BIOTA/Data"
 
 # load in the biome dataset
-library(sf)
 
 # read in the biomes shapefile (this has 4 biomes in, just for forest types?)
 # humid tropics, dry tropics, temperate, boreal
+
+# setting the CRS to world lat/longs
 biomes <- st_read(paste0(datadir, "/biomes/biomes.shp")) %>%
   st_set_crs(4326)
 
+# removing the extra grid code column
+biomes <- biomes[2:3]
+
 # basic plot of the biomes
-plot(biomes['BIOME'])
+plot(biomes)
+
 
 
 # where are the PREDICTS cropland points
@@ -37,19 +46,17 @@ pred <- read.csv(paste0(datadir, "/PREDICTS_2016/Cropland_Sites_SpRichness.csv")
 # extract yield values from the predicts points
 
 # convert pred points into an sf object?
-pred.sf <- st_multipoint(as.matrix(pred[, c("Longitude", "Latitude")]), dim = "XY")
+#pred.sf <- st_multipoint(as.matrix(pred[, c("Longitude", "Latitude")]), dim = "XY")
 
 # add in a CRS?
-pred.sf <- st_sfc(pred.sf, crs = 4326)
+#pred.sf <- st_sf(pred.sf, crs = 4326)
 
 
-library(ggplot2)
-library(ggthemes)
 
 ggplot()+
   borders("world", colour = "black", size = 0.3) +
   theme_map() +
-  geom_sf(data = biomes, aes(fill = BIOME)) +
+  geom_sf(data = biomes, aes(fill = BIOME), alpha = 0.5) +
   geom_sf(data = pred.sf, col = "black") +
   theme(panel.grid.major = element_line(colour = "transparent"))
 
@@ -57,7 +64,35 @@ ggplot()+
 
 # which of the predicts points are in which biome
 
-pred$biome <- st_intersects(biomes['BIOME'], pred.sf)
+# had no idea how to do this, took ages to find an example that I understood:
+# https://gis.stackexchange.com/questions/282750/identify-polygon-containing-point-with-r-sf-package
+
+pred$biome <- apply(pred[ , c("Longitude", "Latitude")], 1, function(x) {  
+  # transformation to palnar is required, since sf library assumes planar projection 
+ 
+  pred.sf <- st_transform(st_sfc(st_point(x),crs = 4326), 4326)
+  # st_intersects with sparse = FALSE returns a logical matrix
+  # with rows corresponds to argument 1 (points) and 
+  # columns to argument 2 (polygons)
+  
+  biomes[which(st_intersects(pred.sf, biomes, sparse = FALSE)), ]$BIOME 
+})
+
+# rename biome values from numbers
+levels(biomes$BIOME)
+pred$biome <- sub(1, "Boreal", pred$biome)
+pred$biome <- sub(2, "Dry Tropics", pred$biome)
+pred$biome <- sub(3, "Humid Tropics", pred$biome)
+pred$biome <- sub(4, "Temperate", pred$biome)
+
+
+ggplot()+
+  borders("world", colour = "black", size = 0.3) +
+  theme_map() +
+  geom_sf(data = biomes, aes(fill = BIOME), alpha = 0.5) +
+  geom_point(data = pred, aes(x = Longitude, y = Latitude, col = biome)) +
+  theme(panel.grid.major = element_line(colour = "transparent"))
+
 
 # remove sites that are not within a forest biome
-
+nrow(pred[pred$biome == "integer(0)",])
--- General_code/Organising_earthstat_data.r
+++ General_code/Organising_earthstat_data.r
@@ -58,7 +58,7 @@ for(file in files){
 # just keep the standardised yield column
 #all_cropdata2 <- all_cropdata[, c("lon", "lat", "crop", "yield_stan")] # if standardising
 #all_cropdata2 <- dcast(all_cropdata, lon +lat ~ crop, value.var = "yield_stan") # if standardising
-all_cropdata2 <- dcast(all_cropdata, lon +lat ~ crop, value.var = "yield")
+all_cropdata2 <- dcast(all_cropdata, lon + lat ~ crop, value.var = "yield")
 
 
 # now calculate total yield per cell
@@ -68,25 +68,7 @@ all_cropdata2$totals <- rowSums(all_cropdata2[, 3:ncol(all_cropdata2)], na.rm = 
 
 # save csv file of the standardised yields and total data
 #write.csv(all_cropdata2, paste0("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/All_Crop_Yield_standardised.csv"))
-write.csv(all_cropdata2, paste0("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/All_Crop_Yield.csv"))
-
-# normality checks on yield totals?
-# histogram of totals
-p1 <- ggplot(all_cropdata2, aes(totals)) +
-  geom_histogram()
-
-# q-q plot of totals
-p2 <- ggplot(all_cropdata2, aes(sample = totals)) +
-  stat_qq() + 
-  stat_qq_line()
-
-# combine into one
-plot_grid(p1, p2) # organise with cowplot function
-
-# save the plot
-ggsave(filename = paste0("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/Normality_check_YieldTotal_Standardised.png"))
-ggsave(filename = paste0("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/Normality_check_YieldTotal.png"))
-
+write.csv(all_cropdata2, paste0("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/All_Crop_Yield.csv"), row.names = F)
 
 # plot total yield on a global map
 
@@ -109,3 +91,178 @@ ggplot(all_cropdata2, aes(x = lon, y = lat))+
 #        width = 8, height = 4)
 ggsave(filename = "D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/Map_YieldTotals.png",
        width = 8, height = 4)
+
+
+
+############################################################
+#                                                          #
+#                  Now to try to match up                  #
+#          the yield data with the PREDICTS data           #
+#                                                          #
+############################################################
+
+# read in the PREDICTS data from the NHM webpage
+pred.all <- read.csv("D:/BIOTA/PREDICTS_2016/resource.csv")
+
+
+
+# subset to cropland sites only
+crop.data <- pred.all[pred.all$Predominant_land_use == "Cropland", ]
+
+
+### to start with, just try to look at species richness per site ###
+
+# subset to useful columns
+crop.data <- crop.data[, c(6:10, 19:21, 27, 31:34, 38:39, 65:68)]
+
+# proportion of records that are non-zero
+1-nrow(crop.data[crop.data$Measurement == 0,])/nrow(crop.data)
+
+
+#### summarise to site level information on species richness ####
+
+  # where to save the outputs
+  all.res <- NULL
+  
+# for each SSBS get the main info and the species richness of the site, save in new table
+  for(site in unique(x$SSBS)){
+    
+    # subset to one site
+    sub.data <- x[x$SSBS == site, ]
+    
+    # extract information and the number of species
+    result <- unlist(c(sub.data[1, c(1, 6:8, 11:15)], nrow(sub.data)))
+    
+    # add into table
+    all.res <- rbind(all.res, result)
+    
+  }
+  
+# add column name for species richness stuff
+  colnames(all.res)[10] <- "Sp.richness"
+  
+  # convert to dataframe
+  
+  all.res <- as.data.frame(all.res)
+
+# save this dataset
+  write.csv(all.res, file = "D:/BIOTA/PREDICTS_2016/Cropland_Sites_SpRichness.csv", row.names = F)
+
+
+
+############################################################
+#                                                          #
+#          Try to match up points with yield data          #
+#                                                          #
+############################################################
+
+
+# read in yield data 
+yield <- read.csv("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/All_Crop_Yield.csv")
+
+# for some reason, reading in the data means it won't plot (?!)  
+# yield <- all_cropdata2
+
+# read in PREDICTS cropland data
+pred <- read.csv("D:/BIOTA/PREDICTS_2016/Cropland_Sites_SpRichness.csv")
+
+
+library(RColorBrewer)
+library(ggplot2)
+library(ggthemes)
+
+cols<-brewer.pal(4, "OrRd")
+
+ggplot(data = yield, aes(x = lon, y = lat))+
+  borders("world", colour = "gray40", size = 0.3) +
+  theme_map() +
+  geom_raster(data = yield, aes(x = lon, y = lat, fill=totals), inherit.aes = FALSE) +
+  geom_point(data = pred, aes( x = Longitude, y = Latitude), col = "green") +
+  #scale_fill_manual(values=cols) +
+  #  guides(fill=guide_legend(title="Yield \n(standardised)")) +
+  guides(fill=guide_legend(title="Yield \n(Tons per hectare)")) +
+  labs(title = paste("Total yield (17 crops), Earthstat")) +
+  theme(legend.position="bottom")+
+  theme_void()+ coord_equal()
+
+ggsave(file = paste0("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/Overlay_yield_PREDICTS.png"))
+
+
+### figure out the yield values at the point coordinates ###
+
+# first, convert both to spatial data objects with the same CRS
+
+library(sf)
+
+# remove the rows that have NAs in the lat/long columns
+pred <- pred[!is.na(pred$Latitude), ]
+
+
+library(raster)
+
+# convert the lat/long/value data into a raster
+yield.sp <- rasterFromXYZ(yield[, c(1,2,20)])  # add a buffer to reduce NAs?
+
+# extract yield values from the predicts points
+yield.vals <- extract(yield.sp, pred[, c("Longitude","Latitude")])
+
+# there are some NAs, how many?
+sum(is.na(yield.vals)) # 109 NAs
+
+
+# reorganise data
+pred$total.yield <- yield.vals
+
+
+# normality checks on yield totals?
+# histogram of totals
+p1 <- ggplot(pred, aes(total.yield)) +
+  geom_histogram()
+
+# q-q plot of totals
+p2 <- ggplot(pred, aes(sample = log(total.yield))) +
+  stat_qq() + 
+  stat_qq_line()
+
+library(cowplot)
+# combine into one
+plot_grid(p1, p2) # organise with cowplot function
+
+# save the plot
+#ggsave(filename = paste0("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/Normality_check_YieldTotal_Standardised.png"))
+ggsave(filename = paste0("D:/BIOTA/1_Forest_Cover_Yield/Data Exploration/Normality_check_YieldTotal.png"))
+
+
+library(lme4)
+
+random1 <- lmer(Sp.richness~total.yield+(1|SS), data=pred)
+
+summary(random1)
+
+random2 <- lmer(Sp.richness~total.yield+(1|SS)+(1|SSB), data=pred)
+
+summary(random2)
+
+# checking which is better
+AIC(random1,random2)
+
+
+### trying out some plotting ###
+library(devtools)
+install_github("timnewbold/StatisticalModels")
+
+library(StatisticalModels)
+
+PlotGLMERContinuous(model = random2, data = pred, effects = "total.yield", xlab = "Total crop yield",
+                    ylab = "Species Richness", line.cols = c("#79CDCD"), 
+outDir = "D:/BIOTA/1_Forest_Cover_Yield/Data Exploration")
+
+
+### testing for over-dispersion ###
+
+GLMEROverdispersion(model = random2)
+
+# dont think there is overdispersion but testing another model anyway.
+
+random3 <- lmer(logAbun~Use_intensity+(1|SS)+(1|SSB)+(1|SSBS), data=model.data) # doesn't work
+
